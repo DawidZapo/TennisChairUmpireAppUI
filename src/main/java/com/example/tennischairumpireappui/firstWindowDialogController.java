0b36f3722e852fcd46b2c7ec8be11ba141b91d04
DawidZapo <dawidzapo@gmail.com>
2023-08-08 00:23:10 +0200

    Both sides symmetric, mirror flip.

--- src/main/java/com/example/tennischairumpireappui/firstWindowDialogController.java
+++ src/main/java/com/example/tennischairumpireappui/firstWindowDialogController.java
@@ -2,13 +2,22 @@ package com.example.tennischairumpireappui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class firstWindowDialogController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private ImageView leftAD, leftDE, rightAD, rightDE;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    public void initialize(){
+        leftDE.setVisible(false);
+        rightDE.setVisible(false);
     }
 }
